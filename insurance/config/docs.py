9603dcf5318727861fa421adfefb43f306249088
1byzero <amitkp66@live.com>
2024-08-12 12:43:44 +0530

    minor doc update

---
+++ insurance/config/docs.py
@@ -0,0 +1,11 @@
+"""
+Configuration for docs
+"""
+
+# source_link = "https://github.com/1byZero/Insurance-App"
+# headline = "Insurance App for Healthcare"
+# sub_heading = "Insurance management and payments made easy"
+
+
+def get_context(context):
+	context.brand_html = "Insurance"
